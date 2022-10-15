9631cd4134b7bd5da88d0402bce0157b32570b54
bersling <bersling@gmail.com>
2022-10-15 11:59:17 +0200

    better run script

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,5 @@
+export RUST_BACKTRACE=1
+docker stop rust-bert || true
 docker run -d --rm -p 8000:8000 --name rust-bert bersling/rust-bert-cpu
 curl localhost:8000/api/health-check
 curl localhost:8000/api/health-check-zero-shot
