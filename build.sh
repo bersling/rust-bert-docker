fef610dd328660c44d4cd9f29df626efd475d19e
bersling <bersling@gmail.com>
2022-10-15 09:44:12 +0200

    add ok flag for failing stop

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ echo "## Crash on errors and unset variables ##"
 set -e
 set -u
 
-docker stop rust-bert
+docker stop rust-bert || true
 docker system prune --force
 docker build --no-cache --name rust-bert -t bersling/rust-bert-cpu -f cpu.Dockerfile .
