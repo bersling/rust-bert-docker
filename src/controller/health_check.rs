ce0ecc0f6707b17f47f020ecfd15d15300ae741f
bersling <bersling@gmail.com>
2022-10-15 13:17:42 +0200

    WIP try to add device

--- src/controller/health_check.rs
+++ src/controller/health_check.rs
@@ -1,5 +1,9 @@
+use rust_bert::distilbert::{DistilBertConfigResources, DistilBertModelResources, DistilBertVocabResources};
+use rust_bert::pipelines::common::ModelType;
+use rust_bert::pipelines::common::ModelType::DistilBert;
 use rust_bert::pipelines::sequence_classification::Label;
-use rust_bert::pipelines::zero_shot_classification::ZeroShotClassificationModel;
+use rust_bert::pipelines::zero_shot_classification::{ZeroShotClassificationConfig, ZeroShotClassificationModel};
+use rust_bert::resources::RemoteResource;
 
 use blocking::unblock;
 use rocket::serde::json::Json;
@@ -28,8 +32,25 @@ pub fn health_check() -> Json<HealthCheckResponse> {
 #[get("/health-check-zero-shot")]
 pub async fn health_check_zero_shot() -> Json<ZeroShotResponse> {
 
-
     let result: Vec<Vec<Label>> = unblock(|| {
+        let config = ZeroShotClassificationConfig {
+            model_type: ModelType::DistilBert,
+            model_resource: Box::new(RemoteResource::from_pretrained(
+                DistilBertModelResources::DISTIL_BERT,
+            )),
+            config_resource: Box::new(RemoteResource::from_pretrained(
+                DistilBertConfigResources::DISTIL_BERT,
+            )),
+            vocab_resource: Box::new(RemoteResource::from_pretrained(
+                DistilBertVocabResources::DISTIL_BERT,
+            )),
+            merges_resource: None, // is this ok?
+            lower_case: false,
+            strip_accents: None,
+            add_prefix_space: None,
+            device: // FIXME: how can i pass device?
+        };
+
         let sequence_classification_model = ZeroShotClassificationModel::new(Default::default()).unwrap();
 
         let input_sentence = "Who are you voting for in 2020?";
